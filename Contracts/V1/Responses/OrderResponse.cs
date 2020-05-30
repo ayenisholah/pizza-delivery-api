a9635992e61ec2fe2d06f455a82f64b1f061dec2
Shola Ayeni <ayenisholah@yahoo.com>
2020-05-30 22:24:01 +0100

    created order response

--- Contracts/V1/Responses/OrderResponse.cs
+++ Contracts/V1/Responses/OrderResponse.cs
@@ -3,8 +3,7 @@ namespace DeliveryAPI.Contracts.V1.Responses
 {
     public class OrderResponse
     {
-        public OrderResponse()
-        {
-        }
+        public string Id { get; set; }
+
     }
 }
