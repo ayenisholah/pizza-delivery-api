f27ce3ea830d131fa8e92a382c035a990105e3ea
Shola Ayeni <ayenisholah@yahoo.com>
2020-05-30 21:35:36 +0100

    moved mvc configuration and services to it's own file

--- Installers/MvcInstaller.cs
+++ Installers/MvcInstaller.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
 namespace DeliveryAPI.Installers
 {
-    public class MvcInstaller
+    public class MvcInstaller : IInstaller
     {
-        public MvcInstaller()
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
+            services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Delivery API", Version = "v1" });
+            });
         }
     }
 }
