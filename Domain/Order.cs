44a266eb77b30ea285894788ec957445800e9de9
Shola <ayenisholah@yahoo.com>
2020-06-01 14:50:23 +0100

    marked the id as primary key to be autogenerated

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DeliveryAPI.Domain
 {
     public class Order
     {
+        [Key]
         public Guid Id { get; set; }
+
         public decimal Amount { get; set; }
     }
 }
