e7f572f972113c6187cd38a0059dfcd293ba85a5
Shola <ayenisholah@yahoo.com>
2020-06-01 23:53:04 +0100

    created cosmos db dto

--- Domain/CosmosOrderDto.cs
+++ Domain/CosmosOrderDto.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Cosmonaut.Attributes;
+using Newtonsoft.Json;
+
 namespace DeliveryAPI.Domain
 {
+    [CosmosCollection("orders")]
     public class CosmosOrderDto
-    {
-        public CosmosOrderDto()
-        {
-        }
+    { 
+        [CosmosPartitionKey]
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        public decimal Amount { get; set; }
+
     }
 }
