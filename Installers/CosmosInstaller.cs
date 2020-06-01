9ef122c8936e0c938332905a8b87d3dbf5e484dd
Shola <ayenisholah@yahoo.com>
2020-06-01 23:52:09 +0100

    cosmos db configuration

--- Installers/CosmosInstaller.cs
+++ Installers/CosmosInstaller.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using Cosmonaut;
+using DeliveryAPI.Domain;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Cosmonaut.Extensions.Microsoft.DependencyInjection;
+
 namespace DeliveryAPI.Installers
 {
-    public class CosmosInstaller
+    public class CosmosInstaller : IInstaller
     {
-        public CosmosInstaller()
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
+            var cosmosStoreSettings = new CosmosStoreSettings(
+                configuration["CosmosSettings:DatabaseName"],
+                configuration["CosmosSettings:AccountUri"],
+                configuration["CosmosSettings:AccountKey"],
+                new ConnectionPolicy { ConnectionMode = ConnectionMode.Direct, ConnectionProtocol = Protocol.Tcp }
+                );
+            services.AddCosmosStore<CosmosOrderDto>(cosmosStoreSettings);
         }
     }
 }
