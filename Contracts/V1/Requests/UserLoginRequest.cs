56fb83a9dda247ab46ca7a4f230d94fcfe335571
Shola <ayenisholah@yahoo.com>
2020-06-04 14:30:44 +0100

    created user login request object

---
+++ Contracts/V1/Requests/UserLoginRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DeliveryAPI.Contracts.V1.Requests
+{
+    public class UserLoginRequest
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
