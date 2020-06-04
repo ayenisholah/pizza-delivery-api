fe991d2db7cff7cfa59294bad1fa8da048ff7e9d
Shola <ayenisholah@yahoo.com>
2020-06-04 13:55:59 +0100

    implemented orders authorization scheme

--- Controllers/V1/IdentityController.cs
+++ Controllers/V1/IdentityController.cs
@@ -1,10 +1,38 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DeliveryAPI.Contracts;
+using DeliveryAPI.Contracts.V1.Requests;
+using DeliveryAPI.Contracts.V1.Responses;
+using DeliveryAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DeliveryAPI.Controllers.V1
 {
-    public class IdentityController
+    public class IdentityController : Controller
     {
-        public IdentityController()
+        private readonly IIdentityService _identityService;
+        public IdentityController(IIdentityService identityService)
         {
+            _identityService = identityService;
+        }
+
+        [HttpPost(ApiRoutes.Identity.Register)]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationRequest request)
+        {
+            var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
+
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+
+            return Ok(new AuthSucessResponse {
+                Token = authResponse.Token
+            });
         }
     }
 }
