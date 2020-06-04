ce004f7bfbf5c7ccec3a9a1b0ba8df784dafd97a
Shola <ayenisholah@yahoo.com>
2020-06-04 14:33:30 +0100

    created login interface

--- Services/IIdentityService.cs
+++ Services/IIdentityService.cs
@@ -7,5 +7,8 @@ namespace DeliveryAPI.Services
     public interface IIdentityService
     {
         Task<AuthenticationResult> RegisterAsync(string email, string password);
+
+        Task<AuthenticationResult> LoginAsync(string email, string password);
+
     }
 }
