b1a8b5b06e6997dbfb410db75cffc730d9a1c868
Shola <ayenisholah@yahoo.com>
2020-06-04 15:52:21 +0100

    created method to get user id from http context

--- Extensions/GeneralExtensions.cs
+++ Extensions/GeneralExtensions.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
 namespace DeliveryAPI.Extensions
 {
-    public class GeneralExtensions
+    public static class GeneralExtensions
     {
-        public GeneralExtensions()
+        public static string GetUserId(this HttpContext httpContext)
         {
+            if (httpContext.User == null)
+            {
+                return string.Empty;
+            }
+
+            return httpContext.User.Claims.Single(x => x.Type == "id").Value;
         }
     }
 }
