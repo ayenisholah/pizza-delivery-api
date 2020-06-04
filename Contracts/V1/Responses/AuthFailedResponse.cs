ed8c21a04dcc04926b51f72d534f9f882007a6fe
Shola <ayenisholah@yahoo.com>
2020-06-04 13:52:09 +0100

    created failed authentical response object

--- Contracts/V1/Responses/AuthFailedResponse.cs
+++ Contracts/V1/Responses/AuthFailedResponse.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace DeliveryAPI.Contracts.V1.Responses
 {
     public class AuthFailedResponse
     {
-        public AuthFailedResponse()
-        {
-        }
+        public IEnumerable<string> Errors { get; set; }
     }
 }
