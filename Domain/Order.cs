13b8871faba32e1b415ee61e6dd62c7af43d24f7
Shola <ayenisholah@yahoo.com>
2020-06-04 15:51:33 +0100

    added userid foreign key to order

--- Domain/Order.cs
+++ Domain/Order.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace DeliveryAPI.Domain
 {
@@ -9,5 +11,10 @@ namespace DeliveryAPI.Domain
         public Guid Id { get; set; }
 
         public decimal Amount { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
     }
 }
