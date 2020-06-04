d2241db63c612e64b5bab3360dcea1e76a281a58
Shola <ayenisholah@yahoo.com>
2020-06-04 14:31:34 +0100

    enforced valid email address

--- Contracts/V1/Requests/UserRegistrationRequest.cs
+++ Contracts/V1/Requests/UserRegistrationRequest.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DeliveryAPI.Contracts.V1.Requests
 {
     public class UserRegistrationRequest
     {
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
