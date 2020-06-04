00c45d48dd216e6bbb28c4a64be3ce5d0953910a
Shola <ayenisholah@yahoo.com>
2020-06-04 14:32:39 +0100

    implemented login method

--- Controllers/V1/IdentityController.cs
+++ Controllers/V1/IdentityController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DeliveryAPI.Contracts;
 using DeliveryAPI.Contracts.V1.Requests;
@@ -19,6 +20,14 @@ namespace DeliveryAPI.Controllers.V1
         [HttpPost(ApiRoutes.Identity.Register)]
         public async Task<IActionResult> Register([FromBody] UserRegistrationRequest request)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage))
+
+                });
+            }
             var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
 
             if (!authResponse.Success)
@@ -30,7 +39,28 @@ namespace DeliveryAPI.Controllers.V1
             }
 
 
-            return Ok(new AuthSucessResponse {
+            return Ok(new AuthSucessResponse
+            {
+                Token = authResponse.Token
+            });
+        }
+
+        [HttpPost(ApiRoutes.Identity.Login)]
+        public async Task<IActionResult> Login([FromBody] UserLoginRequest request)
+        {
+            var authResponse = await _identityService.LoginAsync(request.Email, request.Password);
+
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+
+            return Ok(new AuthSucessResponse
+            {
                 Token = authResponse.Token
             });
         }
