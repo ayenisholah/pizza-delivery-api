e824b474d0c5d7eb2a9f69587e1c0389965922d1
Shola <ayenisholah@yahoo.com>
2020-06-04 14:01:55 +0100

    created identity service interface

--- Services/IIdentityService.cs
+++ Services/IIdentityService.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DeliveryAPI.Domain;
+
 namespace DeliveryAPI.Services
 {
     public interface IIdentityService
     {
+        Task<AuthenticationResult> RegisterAsync(string email, string password);
     }
 }
