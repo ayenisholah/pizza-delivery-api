5bd764f33e1eee77c22ccd1d3081da02e095e1f2
Shola <ayenisholah@yahoo.com>
2020-06-08 00:33:36 +0100

    added refresh token

--- Options/JwtSettings.cs
+++ Options/JwtSettings.cs
@@ -4,5 +4,7 @@ namespace DeliveryAPI.Options
     public class JwtSettings
     {
         public string Secret { get; set; }
+
+        public TimeSpan TokenLifetime { get; internal set; }
     }
 }
