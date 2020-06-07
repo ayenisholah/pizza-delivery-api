3dca2d736bde507ee5bb6c1f9a4dab5bfb64127f
Shola <ayenisholah@yahoo.com>
2020-06-08 00:30:26 +0100

    added refresh token

--- Domain/AuthenticationResult.cs
+++ Domain/AuthenticationResult.cs
@@ -10,5 +10,7 @@ namespace DeliveryAPI.Domain
         public bool Success { get; set; }
 
         public IEnumerable<string> Errors { get; set; }
+
+        public string RefreshToken { get; set; }
     }
 }
