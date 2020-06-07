e34af6c3da409ed677f15f0733d42b9ae570ecb3
Shola <ayenisholah@yahoo.com>
2020-06-08 00:31:18 +0100

    created refresh token table schema

--- Domain/RefreshToken.cs
+++ Domain/RefreshToken.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
 namespace DeliveryAPI.Domain
 {
     public class RefreshToken
     {
-        public RefreshToken()
-        {
-        }
+        [Key]
+        public string Token { get; set; }
+
+        public string JwtId { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ExpiryDate { get; set; }
+
+        public bool Used { get; set; }
+
+        public bool Invalidated { get; set; }
+
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
     }
 }
