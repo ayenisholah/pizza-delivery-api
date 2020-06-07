e75f6e48481da879ccb503b7fefacd59add09ac0
Shola <ayenisholah@yahoo.com>
2020-06-08 00:28:58 +0100

    configured refresh token

--- Controllers/V1/IdentityController.cs
+++ Controllers/V1/IdentityController.cs
@@ -41,7 +41,8 @@ namespace DeliveryAPI.Controllers.V1
 
             return Ok(new AuthSucessResponse
             {
-                Token = authResponse.Token
+                Token = authResponse.Token,
+                RefreshToken= authResponse.RefreshToken
             });
         }
 
@@ -61,7 +62,29 @@ namespace DeliveryAPI.Controllers.V1
 
             return Ok(new AuthSucessResponse
             {
-                Token = authResponse.Token
+                Token = authResponse.Token,
+                RefreshToken = authResponse.RefreshToken
+            });
+        }
+
+        [HttpPost(ApiRoutes.Identity.Refresh)]
+        public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequest request)
+        {
+            var authResponse = await _identityService.RefreshTokenAsync(request.Token, request.RefreshToken);
+
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+
+
+            return Ok(new AuthSucessResponse
+            {
+                Token = authResponse.Token,
+                RefreshToken = authResponse.RefreshToken
             });
         }
     }
